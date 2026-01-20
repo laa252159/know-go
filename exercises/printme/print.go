4dd1e86a159ff179f4e6a27e9029ce1d99aeef91
sanix <laa252159@gmail.com>
2026-01-20 07:08:51 +0400

    PrintAnythingTo

--- exercises/printme/print.go
+++ exercises/printme/print.go
@@ -8,8 +8,8 @@ import (
 // Your PrintAnythingTo function goes here!
 
 func PrintAnythingTo[T any](w io.Writer, p T) {
-	fprint, err := fmt.Fprint(w, p)
-	fmt.Println(fprint, err)
+	bytes, err := fmt.Fprint(w, p)
+	fmt.Println(bytes, err)
 	if err != nil {
 		return
 	}
